4b310c07ccea8eb3d05019f0a39e0d50021161ab
Zinc-ion <96803357+Zinc-ion@users.noreply.github.com>
2024-05-20 14:13:21 +0800

    initialize

---
+++ src/main/java/com/neusoft/neu6053/Neu6053Application.java
@@ -0,0 +1,13 @@
+package com.neusoft.neu6053;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neu6053Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Neu6053Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/neusoft/neu6053/Neu6053ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neusoft.neu6053;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Neu6053ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
