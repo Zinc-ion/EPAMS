c6600895741fd35777d553e0ee6e82dc036da711
Zinc-ion <96803357+Zinc-ion@users.noreply.github.com>
2024-05-20 15:38:19 +0800

    initialize

---
+++ src/main/java/com/neusoft/neu6053/demos/web/test/index.java
@@ -0,0 +1,15 @@
+package com.neusoft.neu6053.demos.web.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class index {
+
+    @RequestMapping("/index")
+    @ResponseBody
+    public String index(){
+        return "hello world!";
+    }
+}
