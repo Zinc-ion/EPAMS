bd1499baf7e12b42c5b2dd8b782b0c12b4c1efb7
Zinc-ion <96803357+Zinc-ion@users.noreply.github.com>
2024-05-30 10:16:17 +0800

    配置Redis，新增Redis配置类，redis工具类，testController中测试方法

---
+++ src/main/java/com/neusoft/neu6053/config/RedisConfig.java
@@ -0,0 +1,100 @@
+package com.neusoft.neu6053.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.*;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> template(RedisConnectionFactory factory) {
+        //创建RedisTemplate<String,Object>对象
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        //配置连接工厂
+        template.setConnectionFactory(factory);
+        //定义Jackson2JsonRedisSerializer序列化对象
+        Jackson2JsonRedisSerializer<Object> jsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        //指定要序列化的域，field,get和set,以及修饰符范围，ANY是包括private到public
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        //指定序列化输入的类型，类必须是非final修饰的，final修饰的类如String,Integer会报异常
+        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
+        jsonRedisSerializer.setObjectMapper(objectMapper);
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+
+        // 值采用json序列化
+        template.setValueSerializer(jsonRedisSerializer);
+        //使用StringRedisSerializer来序列化和反序列化redis的key值
+        template.setKeySerializer(new StringRedisSerializer());
+
+        // 设置hash key 和value序列化模式
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(jsonRedisSerializer);
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    //RedisTemplate<K,V>类以下方法，返回值分别对应操作Redis的String、List、Set、Hash等，可以将这些operations 注入到Spring中，方便使用
+    /**
+     * 对hash类型的数据操作
+     *
+     * @param redisTemplate
+     * @return
+     */
+    @Bean
+    public HashOperations<String, String, Object> hashOperations(RedisTemplate<String, Object> redisTemplate) {
+        return redisTemplate.opsForHash();
+    }
+
+    /**
+     * 对redis字符串类型数据操作
+     *
+     * @param redisTemplate
+     * @return
+     */
+    @Bean
+    public ValueOperations<String, Object> valueOperations(RedisTemplate<String, Object> redisTemplate) {
+        return redisTemplate.opsForValue();
+    }
+
+    /**
+     * 对链表类型的数据操作
+     *
+     * @param redisTemplate
+     * @return
+     */
+    @Bean
+    public ListOperations<String, Object> listOperations(RedisTemplate<String, Object> redisTemplate) {
+        return redisTemplate.opsForList();
+    }
+
+    /**
+     * 对无序集合类型的数据操作
+     *
+     * @param redisTemplate
+     * @return
+     */
+    @Bean
+    public SetOperations<String, Object> setOperations(RedisTemplate<String, Object> redisTemplate) {
+        return redisTemplate.opsForSet();
+    }
+
+    /**
+     * 对有序集合类型的数据操作
+     *
+     * @param redisTemplate
+     * @return
+     */
+    @Bean
+    public ZSetOperations<String, Object> zSetOperations(RedisTemplate<String, Object> redisTemplate) {
+        return redisTemplate.opsForZSet();
+    }
+}
--- src/main/java/com/neusoft/neu6053/controller/TestController.java
+++ src/main/java/com/neusoft/neu6053/controller/TestController.java
@@ -1,7 +1,12 @@
 package com.neusoft.neu6053.controller;
 
+import com.neusoft.neu6053.utils.RedisUtils;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/api")
@@ -16,5 +21,31 @@ public class TestController {
     @PostMapping("/testPost")
     public String testPost(@RequestBody String body) {
         return "This is a test POST API. You posted: " + body;
+    }
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Autowired
+    private RedisUtils redisUtils;
+
+    @GetMapping("/testRedisSet")
+    @ResponseBody
+    public void testRedisSet() {
+        //调用配置类保存key2
+        redisTemplate.opsForValue().set("key2", "Hello Redis2");
+        //调用工具类保存key3并设置过期时间30分钟
+        redisUtils.set("key3", "Hello Redis3",30, TimeUnit.MINUTES);
+        System.out.println("==========key2:" + redisUtils.get("key2") + " expireTime:" + redisUtils.getExpire("key2"));
+        System.out.println("==========key3:" + redisUtils.get("key3") + " expireTime:" + redisUtils.getExpire("key3"));
+    }
+
+    @GetMapping("/testRedisDel")
+    @ResponseBody
+    public void testRedisDel() {
+        System.out.println("==========key2:" + redisUtils.get("key2") + " expireTime:" + redisUtils.getExpire("key2"));
+        System.out.println("==========key3:" + redisUtils.get("key3") + " expireTime:" + redisUtils.getExpire("key3"));
+        redisUtils.del("key2");
+        redisUtils.del("key3");
     }
 }
