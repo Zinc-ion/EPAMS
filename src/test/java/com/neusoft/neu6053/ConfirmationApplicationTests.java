4cc35346ede7af26cc69a1da32b413ff825bd915
Zinc-ion <96803357+Zinc-ion@users.noreply.github.com>
2024-06-07 10:18:19 +0800

    新增aqi反馈与检测的测试类

--- src/test/java/com/neusoft/neu6053/ConfirmationApplicationTests.java
+++ src/test/java/com/neusoft/neu6053/ConfirmationApplicationTests.java
@@ -5,8 +5,14 @@ import com.neusoft.neu6053.services.ConfirmationService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Date;
 
 @SpringBootTest
+@Transactional
+@Rollback
 public class ConfirmationApplicationTests {
     @Autowired
     private ConfirmationService confirmationService;
@@ -18,19 +24,58 @@ public class ConfirmationApplicationTests {
     @Test
     public void testAddConfirmation() {
         Confirmation confirmation = new Confirmation();
-        confirmation.setInformationId(-1);
+        confirmation.setInformationId(-2);
         confirmation.setInspectorName("张三");
         confirmation.setSupervisorName("李四");
-        confirmation.setProvince("北京市");
-        confirmation.setCity("北京");
-        confirmation.setCommunity("海淀区");
-        confirmation.setPollutionLevel("轻度污染 二级");
+        confirmation.setProvince("辽宁");
+        confirmation.setCity("沈阳市");
+        confirmation.setCommunity("浑南区");
+        confirmation.setPollutionLevel("2");
         confirmation.setSo2(0.1);
         confirmation.setCo(3.2);
         confirmation.setPm25(0.1);
         confirmation.setData(java.sql.Date.valueOf("2024-06-06"));
         confirmation.setTime(java.sql.Time.valueOf("15:57:14"));
+        assert confirmationService.addConfirmation(confirmation) == 1;
 
+    }
+
+    @Test
+    public void testDeleteConfirmation() {
+        Confirmation confirmation = new Confirmation();
+        confirmation.setConfId(1);
+        assert confirmationService.deleteConfirmation(confirmation) == 1;
+    }
+
+    @Test
+    public void testUpdateConfirmation() {
+        Confirmation confirmation = new Confirmation();
+        confirmation.setConfId(1);
+        confirmation.setInformationId(-2);
+        confirmation.setInspectorName("张三");
+        confirmation.setSupervisorName("李四");
+        confirmation.setProvince("辽宁");
+        confirmation.setCity("沈阳市");
+        confirmation.setCommunity("浑南区");
+        confirmation.setPollutionLevel("2");
+        confirmation.setSo2(0.1);
+        confirmation.setCo(3.2);
+        confirmation.setPm25(0.1);
+        confirmation.setData(java.sql.Date.valueOf("2024-06-06"));
+        confirmation.setTime(java.sql.Time.valueOf("15:57:14"));
+        assert confirmationService.updateConfirmation(confirmation) == 1;
+    }
+
+    @Test
+    public void testGetConfirmationById() {
+        Confirmation confirmation = new Confirmation();
+        confirmation.setConfId(1);
+        assert confirmationService.getConfirmationById(confirmation) != null;
+    }
+
+    @Test
+    public void testGetAllConfirmations() {
+        assert confirmationService.getAllConfirmations(1, 10) != null;
     }
 
 }
--- src/test/java/com/neusoft/neu6053/InformationApplicationTests.java
+++ src/test/java/com/neusoft/neu6053/InformationApplicationTests.java
@@ -1,7 +1,81 @@
 package com.neusoft.neu6053;
 
+import com.neusoft.neu6053.dao.entity.Information;
+import com.neusoft.neu6053.services.InformationService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @SpringBootTest
+@Transactional
+@Rollback
 public class InformationApplicationTests {
+    @Autowired
+    private InformationService informationService;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void testAddInformation() {
+        Information information = new Information();
+        information.setProvince("湖北省");
+        information.setCity("武汉市");
+        information.setCommunity("洪山区");
+        information.setPollutionLevel("2");
+        information.setData(java.sql.Date.valueOf("2024-06-06"));
+        information.setTime(java.sql.Time.valueOf("15:57:11"));
+        information.setState(0);
+        information.setSupervisorId(-2);
+        information.setInspectorId(-2);
+        information.setFeedback("无");
+        information.setDeleted(0);
+        assert informationService.addInformation(information) == 1;
+    }
+
+    @Test
+    void testDeleteInformation() {
+        Information information = new Information();
+        information.setInformationId(3);
+        assert informationService.deleteInformation(information) == 1;
+    }
+
+    @Test
+    void testUpdateInformation() {
+        Information information = new Information();
+        information.setInformationId(3);
+        information.setProvince("湖北省");
+        information.setCity("武汉市");
+        information.setCommunity("洪山区");
+        information.setPollutionLevel("2");
+        information.setData(java.sql.Date.valueOf("2024-06-06"));
+        information.setTime(java.sql.Time.valueOf("15:57:11"));
+        information.setState(0);
+        information.setSupervisorId(-2);
+        information.setInspectorId(-2);
+        information.setFeedback("无");
+        information.setDeleted(0);
+        assert informationService.updateInformation(information) == 1;
+    }
+
+    @Test
+    void testGetInformationById() {
+        Information information = new Information();
+        information.setInformationId(3);
+        assert informationService.getInformationById(information) != null;
+    }
+
+    @Test
+    void testGetAllInformations() {
+        assert informationService.getAllInformations(1, 10) != null;
+    }
+
+
+
+
+
 }
