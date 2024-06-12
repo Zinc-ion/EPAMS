b5fd18406ab2ed14f6eed971ee554c394a832e84
Zinc-ion <96803357+Zinc-ion@users.noreply.github.com>
2024-06-12 16:08:35 +0800

    修改跨域配置

--- src/main/java/com/neusoft/neu6053/config/filter/CrosFilter.java
+++ src/main/java/com/neusoft/neu6053/config/filter/CrosFilter.java
@@ -1,5 +1,6 @@
 package com.neusoft.neu6053.config.filter;
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -14,15 +15,26 @@ public class CrosFilter implements javax.servlet.Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletResponse res = (HttpServletResponse) servletResponse;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpServletRequest request = (HttpServletRequest)servletRequest;
         //*号表示对所有请求都允许跨域访问
-        res.addHeader("Access-Control-Allow-Origin", "*");
-        res.addHeader("Access-Control-Allow-Methods", "*");
-        res.setHeader("Access-Control-Allow-Credentials", "true");
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         /**
          * Access-Control-Allow-Credentials 响应标头告诉浏览器服务器是否允许 HTTP 跨源请求携带凭据。
          * 凭据包括 cookie、TLS 客户端证书，或包含用户名和密码的认证标头。
          */
+        response.setHeader("Access-Control-Max-Age", "3600");
+        //用于判断request来自ajax还是传统请求
+        response.setHeader("Access-Control-Allow-Headers", " Origin, X-Requested-With, Content-Type, Accept");
+        String method = request.getMethod();
+        if(method.equalsIgnoreCase("OPTIONS")){
+            servletResponse.getOutputStream().write("Success".getBytes("utf-8"));
+        }else{
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
