fefbd8f5bba211549675f7dbe2947f38730ce507
Zinc-ion <96803357+Zinc-ion@users.noreply.github.com>
2024-06-12 15:53:29 +0800

    修改跨域配置

--- src/main/java/com/neusoft/neu6053/config/filter/CrosFilter.java
+++ src/main/java/com/neusoft/neu6053/config/filter/CrosFilter.java
@@ -18,6 +18,11 @@ public class CrosFilter implements javax.servlet.Filter {
         //*号表示对所有请求都允许跨域访问
         res.addHeader("Access-Control-Allow-Origin", "*");
         res.addHeader("Access-Control-Allow-Methods", "*");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        /**
+         * Access-Control-Allow-Credentials 响应标头告诉浏览器服务器是否允许 HTTP 跨源请求携带凭据。
+         * 凭据包括 cookie、TLS 客户端证书，或包含用户名和密码的认证标头。
+         */
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
